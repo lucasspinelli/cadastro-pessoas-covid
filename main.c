25ba749eca6a405b7eaf62f85d25936ccb7a3403
Lucas Levi <lucas.lsl12@gmail.com>
2020-11-15 21:27:32 -0300

    Refaturando todo o código com sistemas de listas

--- main.c
+++ main.c
@@ -3,215 +3,52 @@
 #include<string.h> //Biblioteca para usar a funçao strcmp
 #include<windows.h>
 #include<conio.h>
-#define SIZE 2000
-/* Começo Matriz*/
+#include <locale.h>
 
-char name[SIZE][50];
-char email[SIZE][50];
-int cpf[SIZE][11];
-int birthDay[SIZE][2];
-int birthMonth[SIZE][2];
-int birthYear[SIZE][4];
-int phoneNumber[SIZE][15];
-char street[SIZE][50];
-int streetNumber[SIZE][50];
-char neighborhood[SIZE][50];
-char city[SIZE][20];
-char state[SIZE][5];
-int cep[SIZE][10];
-int diagnosisDay[SIZE][2];
-int diagnosisMonth[SIZE][2];
-int diagnosisYear[SIZE][4];
-char comorbidity[SIZE][20];
-int operation;
-
-/*FIM MATRIZ*/
 
 int main(void){
     /*VARIAVEIS*/
+        FILE *cfPtr;
+
+        int opcao,opcao1,opcao2,opcao3,opcao4,i,qtd;
+
+
+    struct paciente
+    {
+        char nome[100],cpf[30],telefone[15],rua[50],
+        nrua[4],bairro[50],cidade[50],estado[50],cep[20];
+    }
+    CA[600];
+    CA[600];
+    CA[600];
+    CA[600];
+    CA[600];
+    CA[600];
+    CA[600];
+        char usuario[100]; //Variável que guarda o nome do usuário
+        setlocale(LC_ALL,"");
+        system("cls");
         char c;
         char login[20];
         char senha[20];
         int a=10;
-        int i=0;
+        int p=0;
         int verifica_senha=0;
         int quantidade=0;
         int option = 1;
-    /*FiM VARIAVEIS*/
-       /*FUNÇÕES*/
-       int read_option(){ // Ler opções do programa
-            int x;
-            printf("Menu Principal\n");
-            printf("--------------\n");
-            printf("1- Cadastrar Paciente\n");
-            printf("2- Consultar Paciente\n");
-            printf("3- Alterar Paciente\n");
-            printf("4- Excluir Paciente\n");
-            printf("5- Pesquisar Paciente (Nome/CPF)\n");
-            printf("6- Ordenar Paciente - Alfabetica\n");
-            printf("0- Sair do Programa\n");
-            printf(">>> ");
-            scanf("%d",&x);
-            printf("\n");
-            return x;
-        } /*FIM OPCAO*/
 
-        void pause(char mensagem[]){
-            printf("%s",mensagem);
-            getch();
-        } /*FIM PAUSE*/
-
-        void cadastro (){
-            static int line;
-            do {
-
-                printf("Digite o Nome do Paciente\n: ");
-                scanf (" %s", &name[line]);
-
-                fflush(stdin);
-
-                printf("Digite o Email do Paciente:\n ");
-                scanf (" %s", &email[line]);
-
-                fflush(stdin);
-
-                printf("Digite o CPF do Paciente ( Somente Numeros )\n: ");
-                scanf (" %d", &cpf[line]);
-
-                fflush(stdin);
-
-                printf("Qual Dia o Paciente nasceu?(DD):\n ");
-                scanf (" %d", &birthDay[line]);
-
-                fflush(stdin);
-
-                printf("Qual Mes o Paciente nasceu?(MM):\n");
-                scanf (" %d", &birthMonth[line]);
-
-                fflush(stdin);
-
-                printf("Qual Ano o Paciente nasceu?(AAAA):\n ");
-                scanf (" %d", &birthYear[line]);
-
-                fflush(stdin);
-
-                printf("Digite o telefone do Paciente ( Somente Numeros ):\n");
-                scanf (" %d", &phoneNumber[line]);
-
-                fflush(stdin);
-
-                printf("Digite a rua em que o Paciente mora:\n");
-                scanf (" %s", &street[line]);
-
-                fflush(stdin);
-
-                printf("Qual o numero da rua?:\n");
-                scanf (" %d", &streetNumber[line]);
-
-                fflush(stdin);
-
-                printf("Em qual bairro fica essa rua?:\n ");
-                scanf (" %s", &neighborhood[line]);
-
-                fflush(stdin);
-
-                printf("Cidade do Paciente:\n");
-                scanf (" %s", &city[line]);
-
-                fflush(stdin);
-
-                printf("Qual Estado o Paciente mora?:\n");
-                scanf (" %s", &state[line]);
-
-                fflush(stdin);
-
-                printf("CEP do Paciente:\n ");
-                scanf (" %d", &cep[line]);
-
-                fflush(stdin);
-
-                printf("Qual Dia do diagnostico?(DD):\n ");
-                scanf (" %d", &diagnosisDay[line]);
-
-                fflush(stdin);
-
-                printf("Qual Mes do diagnostico?(MM):\n ");
-                scanf (" %d", &diagnosisMonth[line]);
-
-                fflush(stdin);
-
-                printf("Qual Ano do diagnostico?(AAAA)\n: ");
-                scanf (" %d", &diagnosisYear[line]);
-
-                fflush(stdin);
-
-                printf("O paciente tem alguma comorbidade? Se sim, qual?\n");
-                scanf (" %s", &comorbidity[line]);
-                printf("Digite 1 para cadastrar outro paciente, ou 0 para sair:\n  ");
-                scanf (" %d", &operation);
-                line++;
-
-            } while(operation==1);
-        } /*FIM CADASTRO*/
-
-        void search (){ /*Começo pesquisa*/
-
-            int searchCpf;
-            char searchEmail[50];
-            int i;
-
-            do {
-                printf("\n Para pesquisar por CPF digite 1, ou para pesquisar por EMAIL digite 2");
-                scanf("\n%d", &operation);
-
-                switch(operation){
-                case 1:
-                    printf("\n Digite o CPF: ");
-                    scanf("\n%d", &searchCpf);
-                    for (i=0; i>SIZE; i++){
-                        if (cpf[i]==searchCpf){
-                        printf("\nNome: %s\nEmail: %s\nCPF: %d", &name[i], &email[i], &cpf[i]);
-                        }else{
-                            printf("\n NENHUM REGISTRO ENCONTRADO");
-                        }
-                    }
-                    break;
-                case 2:
-                    printf("\n Digite o EMAIL: ");
-                    scanf("\n%s", &searchEmail);
-                    for (i=0; i>SIZE; i++){
-                        if (strcmp(email[i], searchEmail)==0){
-                            printf("\nNome: %s\nEmail: %s\nCPF: %d", &name[i], &email[i], &cpf[i]);
-                        } else{
-                            printf("\n NENHUM REGISTRO ENCONTRADO");
-                        }
-                    }
-
-                    break;
-
-                default:
-                     printf("\n OPCAO INVALIDA");
-                    break;
-                }
-                printf("\nDigite 1 para continuar pesquisando");
-                scanf("%d", &operation);
-            }while(operation==1);
-
-        } /*FIM PESAUISA*/
-
-        /*FIM FUNÇÕES*/
-      while(verifica_senha==0){
+        while(verifica_senha==0){
        printf("\n\t\tCADASTRO DE PACIENTE\n\n login: ");
        //fflush(stdin);  //Limpando o buffer do teclado
-       gets(login);
+       gets(usuario);
        printf("\n senha: ");
        while((c=getch())!=13){ //13 é o valor de ENTER na tabela ASCII
-           senha[i]=c;
-           i++;
+           senha[p]=c;
+           p++;
            printf("*");      //imprime o * Anterisco
            }
-          senha[i]='\0';
-          i=0;
+          senha[p]='\0';
+          p=0;
        system("cls");
        a= strcmp(senha,"chico"); //
        if(a==0){printf("SENHA CORRETA");system("color 0a");verifica_senha=1;Beep(1200,200);}
@@ -223,47 +60,175 @@ int main(void){
        system("color 0F");
        //read_option();
 
-        while (option > 0 && option<=6)
-    {
-        system("CLS");
-        puts("Trabalho PIM IV");
-        puts("* Cadastro de Pacientes *\n");
-        printf("%d Pacientes cadastrados\n\n",quantidade);
+                while(opcao != 6)
+            {
+                while(opcao2 != 1)
+                {
+                    system("color f9");
+                    system("cls");
 
-        option = read_option();
+                    printf("---------------------------------------\n");
+                    printf("\t Seja Bem Vindo(a) Atendente %s\n",usuario);
+                    printf("---------------------------------------\n");
+                    printf(" 1- CADASTRAR PACIÊNTE\n ");
+                    printf("2- EXIBIR DADOS DO PACIÊNTE\n ");
+                    printf("3- INFORMAÇÕES DO SISTEMA\n ");
+                    printf("4- SAIR\n ");
+                    printf("---------------------------------------\n\n");
+                    printf("\nESCOLHA UMA OPÇÃO: ");
+                    scanf("%d",&opcao);
+
+                    switch(opcao)
+                    {
+                    case 1:
+                        system("cls");
+                        printf("\n\n QUANTOS CADASTROS DESEJA FAZER: ");
+                        scanf("%d",&qtd);
+
+                    for (i=1;i<=qtd;i++)
+                        {
+
+                            int ano, idade;
+                            char morbidade;
+                            int anoatual = 2020;
 
 
-        if (option == 0){
-            return 0;
-        }
-        system("CLS");
-        switch (option)
-        {
-           case 1: // 1- Cadastrar Paciente
-               cadastro();
-             break;
 
-           case 2: // 2- Consultar Paciente
-               search();
-             break;
+                            cfPtr = fopen("pacientes.txt","w");
+                            system("cls");
+                            printf("\n\n\t\t\t  INICIANDO CADASTRO %d\n",i);
+                            printf("\t\t\t  ----------------\n\n");
+                            printf("\nDigite o ano de nascimento do paciente: ");
+                            scanf("%d",&ano);
+                            idade = anoatual - ano;
+                            printf("O PACIENTE TEM %d ANOS \n",idade);
 
-           case 3:  // 3- Alterar Paciente
-             break;
+                            if(idade >= 65 && morbidade != ""){
+                             printf("O PACIENTE FAZ PARTE DO GRUPO DE RISCO.");
+                            printf("\n NOME DO PACIENTE: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].nome);
+                            printf("\n CPF DO PACIENTE: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].cpf);
+                            printf("\n TELEFONE DO PACIENTE: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].telefone);
+                            printf("\n CEP: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].cep);
+                            printf("\n NOME DA RUA: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].rua);
+                            printf("\n NÚMERO: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].nrua);
+                            printf("\n BAIRRO: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].bairro);
+                            printf("\n CIDADE: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].cidade);
+                            printf("\n ESTADO: ");
+                            fflush(stdin);
+                            scanf("%[^\n]s",CA[i].estado);
 
-           case 4:  // 4- Excluir Paciente
-             break;
+                            while(!feof(stdin) && i<=qtd){
+                            fprintf(cfPtr, "\n NOME DO PACIENTE: %s\n CPF: %s\n TELEFONE: %s\n CEP: %s\n ", CA[i].nome, CA[i].cpf,CA[i].telefone,CA[i].cep );
+                            printf("PRESSIONE CTRL+Z PARA ENCERRAR ");
+                            scanf("%s%s%s",CA[i].estado,CA[i].cidade,&CA[i].bairro);
+                            }
 
-           case 5:  // 5- Pesquisar Paciente
-             break;
 
-           case 6:  // 6- Ordernar Paciente Orderm Alfabética
-             break;
+                            fclose(cfPtr);
+                            printf("\n\n\t\t");
+                            system("pause");
+                            system("cls");
+                            }
+                            else{
+                                printf("O PACIENTE NÃO FAZ PARTE DO GRUPO DE RISCO.");
 
-           default:
-                read_option();
-            break;
-        }
-         pause("\nAperte Enter para voltar ao Menu Principal!");
-    }
+                                printf("\n NOME DO PACIENTE: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].nome);
+                                printf("\n CPF DO PACIENTE: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].cpf);
+                                printf("\n TELEFONE DO PACIENTE: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].telefone);
+                                printf("\n CEP: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].cep);
+                                printf("\n NOME DA RUA: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].rua);
+                                printf("\n NÚMERO: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].nrua);
+                                printf("\n BAIRRO: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].bairro);
+                                printf("\n CIDADE: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].cidade);
+                                printf("\n ESTADO: ");
+                                fflush(stdin);
+                                scanf("%[^\n]s",CA[i].estado);
+
+                                system("pause");
+                                system("cls");
+                            }
+
+                        }
+                        break;
+
+                    case 2:
+                    system("cls");
+                    for (i=1;i<=qtd;i++)
+                        {
+                            system("cls");
+                            printf("\n\n\t\t EXIBINDO CADASTRO DO PACIÊNTE\n\n");
+                            printf("\n NOME DO PACIÊNTE: %s\n",CA[i].nome);
+                            printf("\n CPF DO PACIÊNTE: %s\n",CA[i].cpf);
+                            printf("\n TELEFONE DO PACIÊNTE: %s\n",CA[i].telefone);
+                            printf("\n CEP: %s\n",CA[i].cep);
+                            printf("\n NOME DA RUA: %s\n",CA[i].rua);
+                            printf("\n NÚMERO: %s\n",CA[i].nrua);
+                            printf("\n BAIRRO: %s\n",CA[i].bairro);
+                            printf("\n CIDADE: %s\n",CA[i].cidade);
+                            printf("\n ESTADO: %s\n",CA[i].estado);
+                            system("pause");
+                        }
+                         break;
+
+                    case 3:
+                    system("cls");
+                    printf("TRABALHO REALIZADO POR: \n\n");
+                    printf("Lucas Spinelli Levi RA: \n");
+                    system("pause");
+                    break;
+
+                    case 4:
+                    system("cls");
+                    printf("\n DESEJA SAIR DO SISTEMA? %s\n",usuario);
+                    printf(" 1-SIM\n 2-NÃO");
+                    printf("\n\n");
+                    scanf("%d",&opcao2);
+
+                    switch(opcao2)
+                        {
+                        case 1:
+                            system("cls");
+                            printf("\n\n ATÉ MAIS %s\n",usuario);
+                            system("pause");
+                            return(0);
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+
     return 0;
 }
